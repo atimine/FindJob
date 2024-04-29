b22e09a3d783dede59653c339130df8462efe996
Ati <adhamxudayberganov942@gmail.com>
2024-04-29 11:33:59 +0500

    sort done

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
-let sort = document.querySelectorAll(".hp-seo-list__link"),
-    arr = []
+function sortArr() {
+    let sort = document.querySelectorAll(".hp-seo-list__link"),
+        arr = []
 
 
-for (let i = 0; i < sort.length; i++) {
-    if (sort[i].innerHTML !== 'lorem') {
-        arr.push(sort[i].innerHTML)
-        arr.sort()
-        sort[i].innerHTML = arr[i];
+    for (let i = 0; i < sort.length; i++) {
+        if (sort[i].innerHTML !== 'lorem') {
+            arr.push(sort[i].innerHTML)
+        }
     }
-
+    arr.sort()
+    for (let i = 0; i < sort.length; i++) {
+        if (sort[i].innerHTML !== 'lorem') {
+            sort[i].innerHTML = arr[i]
+        }
+    }
 }
