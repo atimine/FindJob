f4672773f4b92ab634d02e504c51ef19cf1d60c2
abdusharip <abdusharipa9@gmail.com>
2024-05-04 00:14:34 +0500

    added logo workifit

--- js/script.js
+++ js/script.js
@@ -17,17 +17,13 @@ function sortArr() {
 }
 
 ///
-window.addEventListener('scroll', function(){
-    const header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 0)
-  })
+
   
   const scrollBtn =document.querySelector('.scrollToTop-btn')
   window.addEventListener('scroll', () => {
-      scrollBtn.classList.toggle('active', window.scrollY > 500)
+      scrollBtn.classList.toggle('active', window.scrollY > 300)
   })
   scrollBtn.addEventListener('click', () => {
-      document.body.scrollTop = 0
       document.documentElement.scrollTop = 0
   })
   
